5d04b1d2626c15c1118c3b8b44eafed753fbd90c
dpadevteam <162131196+dpadevteam@users.noreply.github.com>
2024-04-13 08:25:24 +0800

    Add files via upload

---
+++ items.js
@@ -0,0 +1,166 @@
+let items = [
+    {
+        "item_name": "GIS Basketball Jersey",
+        "img_src": "gray_banner_gis.png",
+        "alt": "Blue sleeveless basketball jersey",
+        "gender": "unisex",
+        "type": "tanktops",
+        "category": "sportswear",
+        "price": 380,
+        "stock": 16
+    },
+    {
+        "item_name": "San Miguel Volleyball Jersey",
+        "img_src": "gray_banner_san_miguel.png",
+        "alt": "Violet volleyball jersey",
+        "gender": "female",
+        "type": "shirts",
+        "category": "sportswear",
+        "price": 380,
+        "stock": 14
+    },
+    
+    {
+        "item_name": "BMC Hoodie",
+        "img_src": "gray_banner_bmx.png",
+        "alt": "White biking hoodie",
+        "gender": "male",
+        "type": "hoodies",
+        "category": "sportswear",
+        "price": 350,
+        "stock": 0
+    },
+    {
+        "item_name": "Batch 13 Basketball Jersey",
+        "img_src": "gray_banner_javate.png",
+        "alt": "Green basketball jersey with white stripes and orange accents",
+        "gender": "unisex",
+        "type": "tanktops",
+        "category": "sportswear",
+        "price": 380,
+        "stock": 24
+    },
+    {
+        "item_name": "USJR Basketball Jersey",
+        "img_src": "gray_banner_usjr.png",
+        "alt": "Dark green basketball jersey with dark green background and pale accents",
+        "gender": "unisex",
+        "type": "tanktops",
+        "category": "sportswear",
+        "price": 350,
+        "stock": 24
+    },
+    {
+        "item_name": "Nesma Basketball Jersey",
+        "img_src": "gray_banner_nesma.png",
+        "alt": "Basketball jersey with black background and golden brown accents",
+        "gender": "unisex",
+        "type": "combo",
+        "category": "sportswear",
+        "price": 350,
+        "stock": 24
+    },
+    {
+        "item_name": "CNU Balamban Basketball Jersey",
+        "img_src": "gray_banner_cnu.png",
+        "alt": "Basketball jersey with maroon background and yellow accents",
+        "gender": "unisex",
+        "type": "tanktops",
+        "category": "sportswear",
+        "price": 380,
+        "stock": 24
+    },
+    {
+        "item_name": "Matatag Faculty Uniform",
+        "img_src": "gray_banner_matatag.png",
+        "alt": "Polo shirt with white and magenta accents",
+        "gender": "unisex",
+        "type": "shirts",
+        "category": "uniforms",
+        "price": 380,
+        "stock": 24
+    },
+    {
+        "item_name": "Powerplant Technology Uniform",
+        "img_src": "powerplant_big.png",
+        "alt": "Black and white polo shirt with yellow accents",
+        "gender": "unisex",
+        "type": "combo",
+        "category": "uniforms",
+        "price": 380,
+        "stock": 24
+    },
+    {
+        "item_name": "I Love Angel/Devil Couple Shirt",
+        "img_src": "gray_banner_love_angel2.png",
+        "alt": "Pair of T-shirts with white background and black graphics",
+        "gender": "unisex",
+        "type": "shirts",
+        "category": "themed",
+        "price": 350,
+        "stock": 24
+    },
+    {
+        "item_name": "Black / White Couple Hoodie",
+        "img_src": "gray_banner_bwhoodie.png",
+        "alt": "Black and white pair of hoodies",
+        "gender": "unisex",
+        "type": "hoodies",
+        "category": "themed",
+        "price": 380,
+        "stock": 24
+    },
+    {
+        "item_name": "Looc Sur Committee Hoodie",
+        "img_src": "gray_banner_loocsur.png",
+        "alt": "",
+        "gender": "unisex",
+        "type": "hoodies",
+        "category": "themed",
+        "price": 380,
+        "stock": 24
+    },
+    {
+        "item_name": "The Squad Hoodie",
+        "img_src": "gray_banner_squad.png",
+        "alt": "",
+        "gender": "unisex",
+        "type": "hoodies",
+        "category": "themed",
+        "price": 380,
+        "stock": 24
+    },
+    {
+        "item_name": "Oldtown Basketball Hoodie",
+        "img_src": "gray_banner_oldtown.png",
+        "alt": "",
+        "gender": "unisex",
+        "type": "hoodies",
+        "category": "themed",
+        "price": 380,
+        "stock": 24
+    },
+    
+    
+]
+
+let filters = {
+    "gender": {
+        "male": true,
+        "female": true,
+        "unisex": true
+    },
+    "type": {
+        "shirts": true,
+        "tanktops": true,
+        "shorts": true,
+        "pants": true,
+        "hoodies": true,
+        "combo": true
+    },
+    "material": {
+        "polyester": true,
+        "soft": true,
+        "rugged": true
+    }
+}
